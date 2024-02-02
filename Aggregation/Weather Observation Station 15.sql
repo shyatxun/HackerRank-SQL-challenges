9cf851137c0e9c5eabd42d64981dc6a0cde51aa5
shyatxun <157414760+shyatxun@users.noreply.github.com>
2024-02-02 13:07:04 -0800

    Create Weather Observation Station 15.sql

---
+++ Aggregation/Weather Observation Station 15.sql
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
+
+select round(long_w, 4) from station
+where lat_n = (select max(lat_n) from station where lat_n < 137.2345)
