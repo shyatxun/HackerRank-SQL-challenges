076473e4749157edadd10872c1d179c154347457
shyatxun <157414760+shyatxun@users.noreply.github.com>
2024-02-02 13:10:56 -0800

    Create Weather Observation Station 17.sql

---
+++ Aggregation/Weather Observation Station 17.sql
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
+
+select round(long_w, 4) from station
+where lat_n = (select min(lat_n) from station where lat_n > 38.7780)
