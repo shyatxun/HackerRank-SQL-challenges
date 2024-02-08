e5acd9ba3a9b2290a64b97fac4d27e5d98e475e2
shyatxun <157414760+shyatxun@users.noreply.github.com>
2024-02-08 14:17:44 -0800

    Create Challenges.sql

---
+++ Basic Join/Challenges.sql
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/challenges/problem?isFullScreen=true
+
+select hacker_id, name, created from (select h.hacker_id, h.name, count(c.challenge_id) as created, dense_rank() over(order by count(c.challenge_id) desc) as rk from hackers h inner join challenges c on h.hacker_id = c.hacker_id group by h.hacker_id, h.name order by created desc, h.hacker_id) rnk
+where rk = 1 or rk in (select rk from (select h.hacker_id, h.name, count(c.challenge_id) as created, dense_rank() over(order by count(c.challenge_id) desc) as rk from hackers h inner join challenges c on h.hacker_id = c.hacker_id group by h.hacker_id, h.name order by created desc, h.hacker_id) rnk group by rk having count(hacker_id) = 1)
