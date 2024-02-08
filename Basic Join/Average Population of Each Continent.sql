56ea1563792b3e34c6fd41eaea57340acbf896ba
shyatxun <157414760+shyatxun@users.noreply.github.com>
2024-02-08 12:54:58 -0800

    Create Average Population of Each Continent.sql

---
+++ Basic Join/Average Population of Each Continent.sql
@@ -0,0 +1,5 @@
+# https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true
+
+select c2.continent, floor(avg(c1.population)) from city c1
+inner join country c2 on c1.countrycode = c2.code
+group by c2.continent
